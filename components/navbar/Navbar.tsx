2044c42d10dfcfacd383dad50d67b82ca39d7763
Ambar <ambar.aguilar.c@gmail.com>
2024-12-04 18:25:18 -0300

    navbar updated

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon } from '@primer/octicons-react'
+// import { HomeIcon } from '@primer/octicons-react'
 import Link from 'next/link'
 import React from 'react'
 import { ActiveLink } from '../active-link/ActiveLink'
@@ -13,7 +13,7 @@ export const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
       <Link href={'/'} className='flex items-center'>
-        <HomeIcon className='mr-2' />
+        {/* <HomeIcon className='mr-2' /> */}
         <span>Home</span>
       </Link>
 
